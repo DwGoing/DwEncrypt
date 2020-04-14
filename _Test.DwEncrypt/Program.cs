d2b0f0ac5111a85f63445ee20321106668ccf465
DwGoing <jianghy1209@163.com>
2020-04-14 22:59:00 +0800

    添加测试项目

---
+++ _Test.DwEncrypt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using DwEncrypt;
+
+namespace _Test.DwEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
